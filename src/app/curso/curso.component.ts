ff6fe46208e32b5f43468dea3802ad662cf4e9ff
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-16 20:52:13 -0300

    adicionando o httpClientModule

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursoComponent implements OnInit {
   //Construtor
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   //Inicializador
   ngOnInit(): void {}
