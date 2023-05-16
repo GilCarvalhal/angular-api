a9c2c818f8e234642299d6f5a93bea38a497152d
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-16 13:10:41 -0300

    desenvolvendo as funcoes

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -6,13 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./curso.component.css'],
 })
 export class CursoComponent implements OnInit {
-  nome: string = 'Gil';
-
   //Construtor
   constructor() {}
 
   //Inicializador
-  ngOnInit(): void {
-    window.alert('Hello World');
+  ngOnInit(): void {}
+
+  //Cadastro
+  cadastro(): void {
+    alert('Cadastro');
+  }
+
+  //Seleção
+  selecao(): void {
+    alert('Seleção');
+  }
+
+  //Alterar
+  alterar(): void {
+    alert('Alterar');
+  }
+
+  //Remover
+  remover(): void {
+    alert('Remover');
   }
 }
