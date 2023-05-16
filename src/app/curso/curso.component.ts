b9d96257294bb2ff03f7b06a3ba478634dc43d6d
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-15 22:27:47 -0300

    trabalhando com interpolacao de binding e ngOnInit

--- src/app/curso/curso.component.ts
+++ src/app/curso/curso.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-curso',
   templateUrl: './curso.component.html',
-  styleUrls: ['./curso.component.css']
+  styleUrls: ['./curso.component.css'],
 })
-export class CursoComponent {
+export class CursoComponent implements OnInit {
+  nome: string = 'Gil';
 
+  //Construtor
+  constructor() {}
+
+  //Inicializador
+  ngOnInit(): void {
+    window.alert('Hello World');
+  }
 }
