63b7cd44b3c1e1b01c4ba6d054801fe45baaeb94
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-05-16 21:22:55 -0300

    adicionando o httpClientModule no service

--- src/app/curso/curso.service.ts
+++ src/app/curso/curso.service.ts
@@ -1,9 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CursoService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 }
